d87bc567a317602e867b1de20abbc392f6d5aca2
jessy Prananda Ismai <jessy.prananda@gmail.com>
2024-06-20 09:43:54 +0700

    add file system

--- resources/views/app-web.blade.php
+++ resources/views/app-web.blade.php
@@ -13,7 +13,7 @@
     <link
         href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&family=Roboto:wght@400;500;700&display=swap"
         rel="stylesheet">
-    <link rel="stylesheet" href="../node_modules/@fortawesome/fontawesome-free/css/all.min.css">
+    {{-- @vite('node_modules/@fortawesome/fontawesome-free/css/all.min.css') --}}
     @vite('resources/css/app.css')
     @vite('resources/css/main.css')
 </head>
