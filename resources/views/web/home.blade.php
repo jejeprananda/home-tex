7d861d5b563bf608ae40dc6461b784f300617137
jessy Prananda Ismai <jessy.prananda@gmail.com>
2024-06-21 21:32:28 +0700

    8

--- resources/views/web/home.blade.php
+++ resources/views/web/home.blade.php
@@ -152,7 +152,7 @@
     <div class="container pb-16">
         <h2 class="text-2xl font-medium text-gray-800 uppercase mb-6">recomended for you</h2>
         <div class="grid grid-cols-2 md:grid-cols-4 gap-6">
-            @foreach ($products->sortByDesc('created_at')->take(4) as $product)
+            @foreach ($products->sortByDesc('created_at')->take(8) as $product)
                 <div class="bg-white shadow rounded overflow-hidden group">
                     <div class="relative">
                         <a href="products/show/{{ $product->id }}">
